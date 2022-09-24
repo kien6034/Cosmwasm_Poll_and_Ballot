a397a82d663e60bd7c9aaa049a6c7d365190c069
phamminh0811 <1phamminh0811@gmail.com>
2022-09-24 13:29:38 +0700

    Multiple choice contract

--- src/error.rs
+++ src/error.rs
@@ -6,12 +6,22 @@ pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
-    #[error("Custom Error val: {val:?}")]
-    CustomError { val: String },
-
     #[error("PollExisted")]
     PollExisted,
     
     #[error("PollNotExisted")]
-    PollNotExisted
+    PollNotExisted,
+
+    #[error("Answer can't be null")]
+    AnswerCanNotBeNull,
+
+    #[error("Answer nums must be greater than 1, current has {} answers", answer_nums)]
+    AnswerNotEnough{ answer_nums: usize},
+
+    #[error("Must be admin or poll owner to do this")]
+    NoAuthority,
+
+    #[error("Answer {} not found", answers)]
+    ChoiceNotFound{ answers: String},
+
 }
--- src/msg.rs
+++ src/msg.rs
@@ -10,13 +10,34 @@ pub struct InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    
+    CreatePoll {
+        id: String,
+        question: String,
+        answers: Vec<String>
+    },
+    DeletePoll {
+        poll_id: String
+    },
+    Vote {
+        poll_id: String,
+        choice: String
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-   
+    GetPoll{
+        poll_id: String,
+    }, 
+    GetBallot{
+        voter: String,
+        poll_id: String
+    },
+    GetVoteNum{
+        poll_id: String, 
+        choice: String
+    }
 }
 
 // #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
