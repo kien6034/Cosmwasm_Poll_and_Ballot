790907e368cc0d78d3609ad2bf576958cf8c36a6
Long Phu <longphu257@gmail.com>
2022-10-01 18:12:53 +0000

    instantiate contract & test

--- src/contract.rs
+++ src/contract.rs
@@ -1,13 +1,18 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-//use cw2::set_contract_version;
+use cosmwasm_std::{
+    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult,
+};
+use cw2::set_contract_version;
+use crate::state::{Ballot, Config, Poll, BALLOTS, CONFIG, POLLS};
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::msg::{
+    AllPollsResponse, ExecuteMsg, InstantiateMsg, PollResponse, QueryMsg, VoteResponse,
+};
 
-//const CONTRACT_NAME: &str = "crates.io:cw-starter";
-//const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+const CONTRACT_NAME: &str = "crates.io:cw-starter:poll-ballot-multi-options";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
@@ -16,7 +21,17 @@ pub fn instantiate(
     _info: MessageInfo,
     _msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    // unimplemented!()
+    set_contract_version(_deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    let admin = _msg.admin.unwrap_or(_info.sender.to_string()); // if None, use info.sender
+    let validated_admin = _deps.api.addr_validate(&admin)?; // validate the address
+    let config = Config {
+        admin: validated_admin.clone(),
+    };
+    CONFIG.save(_deps.storage, &config)?;
+    Ok(Response::new()
+        .add_attribute("action", "instantiate")
+        .add_attribute("admin", validated_admin.to_string()))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -34,4 +49,56 @@ pub fn execute(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
     unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::contract::{instantiate, execute, query};
+    use crate::msg::{
+        AllPollsResponse, ExecuteMsg, InstantiateMsg, PollResponse, QueryMsg, VoteResponse,
+    };
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
+    use cosmwasm_std::{attr, from_binary};
+    
+    pub const ADDR1: &str = "addr1";
+    #[test]
+    fn test_instantiate_addr1() {
+        // Mock the dependencies, must be mutable so we can pass it as a mutable, empty vector means our contract has no balance
+        let mut deps = mock_dependencies();
+        // Mock the contract environment, contains the block info, contract address, etc.
+        let env = mock_env();
+        // Mock the message info, ADDR1 will be the sender, the empty vec means we sent no funds.
+        let info = mock_info(ADDR1, &[]);
+
+        // Create a message where we (the sender) will be an admin
+        let msg = InstantiateMsg { admin: None };
+        // Call instantiate, unwrap to assert success
+        let res = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(
+            res.attributes,
+            vec![attr("action", "instantiate"), attr("admin", ADDR1)]
+        )
+    }
+    pub const ADDR2: &str = "addr2";
+    #[test]
+    fn test_instantiate_addr2() {
+        // Mock the dependencies, must be mutable so we can pass it as a mutable, empty vector means our contract has no balance
+        let mut deps = mock_dependencies();
+        // Mock the contract environment, contains the block info, contract address, etc.
+        let env = mock_env();
+        // Mock the message info, ADDR2 will be the sender, the empty vec means we sent no funds.
+        let info = mock_info(ADDR2, &[]);
+
+        // Create a message where we (the sender) will be an admin
+        let msg = InstantiateMsg { admin: None };
+        // Call instantiate, unwrap to assert success
+        let res = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(
+            res.attributes,
+            vec![attr("action", "instantiate"), attr("admin", ADDR2)]
+        )
+    }
+    
 }
